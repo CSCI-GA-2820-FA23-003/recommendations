d41e44ebd05deb3218cde4297a4e2cbec8dcb8ba
yujinlee0130 <yujinlee0130@gmail.com>
2023-10-14 21:54:27 +0000

    Adding recommendation factory to create fake data for testing

---
+++ tests/factories.py
@@ -0,0 +1,24 @@
+import factory
+from factory.fuzzy import FuzzyChoice
+from service.models import Recommendation, RecommendationType
+
+
+class RecommendationFactory(factory.Factory):
+    """It creates a fake recommendation for testing"""
+
+    class Meta:
+        model = Recommendation
+
+    id = factory.Sequence(lambda n: n)
+    name = factory.Faker("name")
+    recommendation_id = factory.Sequence(lambda n: n)
+    recommendation_name = factory.Faker("name")
+    type = FuzzyChoice(
+        choices=[
+            RecommendationType.UPSELL,
+            RecommendationType.CROSSSELL,
+            RecommendationType.ACCESSORY,
+        ]
+    )
+    number_of_likes = 0
+    number_of_dislikes = 0
