9ca94c0bf5ff737d7b40250462da5fafcdb2d4f3
yujinlee0130 <yujinlee0130@gmail.com>
2023-11-28 04:27:28 +0000

    added a feature file and recommendation_steps.py

---
+++ features/steps/recommendations_steps.py
@@ -0,0 +1,55 @@
+######################################################################
+# Copyright 2016, 2023 John J. Rofrano. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+######################################################################
+
+"""
+Recommendation Steps
+
+Steps file for recommendations.feature
+
+For information on Waiting until elements are present in the HTML see:
+    https://selenium-python.readthedocs.io/waits.html
+"""
+import requests
+from behave import given
+
+# HTTP Return Codes
+HTTP_200_OK = 200
+HTTP_201_CREATED = 201
+HTTP_204_NO_CONTENT = 204
+
+@given('the following recommendations')
+def step_impl(context):
+    """ Delete all Recommendations and load new ones """
+
+    # List all of the recommendations and delete them one by one
+    rest_endpoint = f"{context.base_url}/recommendations"
+    context.resp = requests.get(rest_endpoint)
+    assert(context.resp.status_code == HTTP_200_OK)
+    for rec in context.resp.json():
+        context.resp = requests.delete(f"{rest_endpoint}/{rec['rec_id']}")
+        assert(context.resp.status_code == HTTP_204_NO_CONTENT)
+
+    # load the database with new pets
+    for row in context.table:
+        payload = {
+            "source_pid": row['source_pid'],
+            "name": row['name'],
+            "recommendation_name": row['recommendation_name'],
+            "type": row['type'],
+            "number_of_likes": row['number_of_likes']
+        }
+        context.resp = requests.post(rest_endpoint, json=payload)
+        assert(context.resp.status_code == HTTP_201_CREATED)
--- service/routes.py
+++ service/routes.py
@@ -29,10 +29,7 @@ def healthcheck():
 @app.route("/")
 def index():
     """Root URL response"""
-    return (
-        "Reminder: return some useful information in json format about the service here",
-        status.HTTP_200_OK,
-    )
+    return app.send_static_file("index.html")
 
 
 ######################################################################
